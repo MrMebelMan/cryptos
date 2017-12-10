807ae6bdba3bac2d79e45a31222f4ff557d121ba
MrMebelMan <burzavla@fit.cvut.cz>
2017-12-10 23:19:55 +0100

    refactored to remove the tmp file

--- cboe.sh
+++ cboe.sh
@@ -9,25 +9,24 @@ trap 'STOP=true' SIGINT SIGQUIT SIGTERM
 while [ "$STOP" != true ];
 do
   clear
-  wget -q -O- "$LINK" > out.tmp
-  
+
   STR='<td width="12%" style="text-align: right;" class="right">'
 
   # Parse price and change fields (the ugly way)
-  DATA=$(cat out.tmp | \
+  DATA=$(wget -q -O- "$LINK" | \
     sed 's/<\/div>/&\n/g' | \
     grep "$STR" | \
     sed "s/$STR//g; s/<\/td>//g" | \
     head -2 | \
-    sed "s/<span class='numeric-negative'>//g; s/<\/span>//g; s/^[ \t]*//g")
+    sed "s/<span class='numeric-negative'>//g" | \
+    sed "s/<\/span>//g; s/^[ \t]*//g")
   
   PRICE=$(echo "$DATA" | head -1)
   CHANGE=$(echo "$DATA" | tail -1)
+
   echo "GXBT: $PRICE"
   echo "Change: $CHANGE"
 
   sleep 3
 done
-
-rm out.tmp
 
